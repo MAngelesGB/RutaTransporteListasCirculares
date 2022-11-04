003619e96863579b681ded4c9cec50a2d1ff9858
MAngelesGB <mgutierrez6@ucol.mx>
2022-11-04 09:40:06 -0600

    metodo agregar

--- ListasCirculares.js
+++ ListasCirculares.js
@@ -14,9 +14,21 @@ class RutaTransporte{
         this.primero = null; 
     }
 
-    agregar()
+    agregar(base)
     {
-
+        if(this.primero === null)
+        {
+            this.primero = base; 
+            base.sig = base;  
+            base.ant = base;  
+        }
+        else
+        {
+            base.sig = this.primero; 
+            base.ant = this.primero.ant; 
+            this.primero.ant.sig = base; 
+            this.primero.ant = base; 
+        }
     }
 
     buscar()
