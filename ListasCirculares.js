8f545577eec323bed9dc6d51715e3b15187c9859
MAngelesGB <mgutierrez6@ucol.mx>
2022-11-04 09:00:46 -0600

    primer version

---
+++ ListasCirculares.js
@@ -0,0 +1,36 @@
+class Base{
+    constructor(nombre, minutos)
+    {
+        this.nombre = nombre; 
+        this.minutos = minutos; 
+        this.ant = null; 
+        this.sig = null; 
+    }
+}
+
+class RutaTransporte{
+    constructor()
+    {
+        this.primero = null; 
+    }
+
+    agregar()
+    {
+
+    }
+
+    buscar()
+    {
+
+    }
+
+    eliminar()
+    {
+
+    }
+
+    imprimir()
+    {
+
+    }
+}
